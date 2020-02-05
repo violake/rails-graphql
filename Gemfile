7b506131eda9822bab676316c0912e2feb882916
Roger Fang <roger.fang@rea-group.com>
2020-02-05 11:29:49 +1100

    feat: add rspec and sample test

--- Gemfile
+++ Gemfile
@@ -15,7 +15,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails', '~> 5.1.1'
   gem 'pry-byebug'
+  gem 'rspec-rails', '3.9.0'
 end
 
 group :development do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'factory_bot_rails'
+
+RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+end
